ebf17bfbeb79e8921ec8f3f548b02b40510ba73e
yujinKim <yujinkimvv@gmail.com>
2025-02-24 20:21:53 +0900

    Build: sonarqube 설정 추가 (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.9"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "com.cantech.batch"
@@ -12,6 +13,14 @@ java {
 		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
+
+sonar {
+	properties {
+		property("sonar.projectKey", "batch-service")
+		property("sonar.projectName", "batch-service")
+	}
+}
+
 
 repositories {
 	mavenCentral()
